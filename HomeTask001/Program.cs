ddfdf32da1807da000bea5b96b49f06cfcf9f850
X-Oleg <Oleg.Kharishin@gmail.com>
2022-08-27 00:32:34 +0300

    Seminar-01 Task-02&04&06&08

--- HomeTask001/Program.cs
+++ HomeTask001/Program.cs
@@ -41,3 +41,21 @@ if( number % 2 == 0 )
     Console.WriteLine($"Task-06  Number {number} is even. => Yes ");
 else
     Console.WriteLine($"Task-06  Number {number} is odd. => No ");
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+int numberN = new Random().Next(1, 30);
+int i = 2;
+
+Console.Write($"Task-08  Number {numberN} ->");
+
+while ( i <= numberN )
+{
+    if(i % 2 == 0)
+        Console.Write($" {i},");
+    i++;
+}
+
