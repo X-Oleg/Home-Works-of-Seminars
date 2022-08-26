97a87a8d01b668518023dfe40670f910e89c3899
X-Oleg <Oleg.Kharishin@gmail.com>
2022-08-26 19:01:44 +0300

    Some intresting code in C#

---
+++ Some_intresting/Program.cs
@@ -0,0 +1,21 @@
+﻿// This is branch Lesson-1
+double a = 50;
+// int b = 70;
+
+Console.Write("Insert Number: ");
+double num = double.Parse(Console.ReadLine()!);
+
+Console.Write("Insert Number2: ");
+if (!(double.TryParse(Console.ReadLine(), out a)))
+    Console.Write("Don't Number!!\n\n");
+else
+{
+    Console.WriteLine(num +" ** " + a + "\n stepen");
+    Console.Write(Math.Pow(num, a)); // num ** a
+    Console.Write($"\n a =  {a}  Number =  {num}\n\n");
+}
+
+
+
+
+
